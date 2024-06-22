0e65bf9a5a7fa2c56954d8bfcbfc11757cbd911d
Juan Eugenio <juan.eugenio@elogroup.com.br>
2024-06-22 02:27:37 -0300

    Iniciando

---
+++ GiveME_Project/src/main/java/com/soce/GiveME/GiveMeApplication.java
@@ -0,0 +1,13 @@
+package com.soce.GiveME;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiveMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiveMeApplication.class, args);
+	}
+
+}
---
+++ GiveME_Project/src/test/java/com/soce/GiveME/GiveMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soce.GiveME;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiveMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
